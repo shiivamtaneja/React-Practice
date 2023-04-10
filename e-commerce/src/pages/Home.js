6e70fa701af4d2380eb289a8cd2d9e03b2e0af1c
ShivamTaneja <shiivamtaneja@gmail.com>
2023-04-10 23:32:03 +0530

    Add Announcement Section

--- e-commerce/src/pages/Home.js
+++ e-commerce/src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Navbar } from '../components/Navbar'
+import Annoucement from '../components/Annoucement'
+import Slider from '../components/Slider'
 
 const Home = () => {
   return (
     <div>
+      <Annoucement />
       <Navbar />
+      <Slider />
     </div>
   )
 }
