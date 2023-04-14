cadf16763be2e5c92bbd824b97294f1ebe505ac6
ShivamTaneja <shiivamtaneja@gmail.com>
2023-04-14 14:01:09 +0530

    Add slide functionality to wrapper

--- e-commerce/src/components/Slider.jsx
+++ e-commerce/src/components/Slider.jsx
@@ -1,11 +1,15 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@mui/icons-material"
+import { useState } from "react"
 import styled from "styled-components"
+
+import { sliderItems } from "../data"
 
 const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
     position: relative;
+    overflow: hidden;
 `
 
 const Arrow = styled.div`
@@ -23,37 +27,44 @@ const Arrow = styled.div`
     left: ${props => props.direction === "left" ? "10px" : undefined};
     right: ${props => props.direction === "right" ? "10px" : undefined};
     cursor: pointer;
-    opacity: 0.5
+    opacity: 0.5;
+    z-index: 2;
 `
 
 const Wrapper = styled.div`
     height: 100%;
+    display: flex;
+    transform: translateX(${props=>props.slideIndex * -100}vw);
 `
 
 const Slide = styled.div`
     width: 100vw;
-    heigh: 100vh;
-    displya: flex;
+    height: 100vh;
+    display: flex;
     align-items: center;
+    background-color: #${props=>props.bg}
 `
 
 const ImgContainer = styled.div`
     height: 100%;
+    display: flex;
+    align-items: center;
     flex: 1;
 `
 
 const Image = styled.img`
-    height: 80%;
+    height: 700px;
+    width: 500px;
+    
 `
 
 const InfoContainer = styled.div`
-    flex: 1;
     padding: 50px;
+    flex: 1;
 `
 
 const Title = styled.h1`
     font-size: 70px;
-
 `
 
 const Description = styled.p`
@@ -64,32 +75,50 @@ const Description = styled.p`
 `
 
 const Button = styled.button`
-    
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer;
 `
 
 const Slider = () => {
-  return (
-    <Container>
-        <Arrow direction="left">
-            <ArrowLeftOutlined />
-        </Arrow>
-        <Wrapper>
-            <Slide>
-                <ImgContainer>
-                    <Image src="https://i.ibb.co/VtkCF1n/9.png"/>
-                </ImgContainer>
-                <InfoContainer>
-                    <Title>SUMMER SALE</Title>
-                    <Description>DON'T COMPROMISE ON STYLE! GET FLAT 69% OFF FOR NEW ARRIVALS.</Description>
-                    <Button>SHOP NOW</Button>
-                </InfoContainer>
-            </Slide>
-        </Wrapper>
-        <Arrow direction="right">
-            <ArrowRightOutlined />
-        </Arrow>
-    </Container>
-  )
+  
+    const [slideIndex, setSlideIndex] = useState(0);
+
+    const handleClick = (direction) => {
+		if(direction === "left"){
+			setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2)
+		}
+		else {
+			setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0)
+		}
+    }
+    return (
+        <Container>
+            <Arrow direction="left" onClick={()=>handleClick("left")}>
+                <ArrowLeftOutlined />
+            </Arrow>
+            <Wrapper slideIndex={slideIndex}>
+				{sliderItems.map( (item) =>{
+					return (
+						<Slide bg={item.bg}>
+							<ImgContainer>
+								<Image src={item.img}/>
+							</ImgContainer>
+							<InfoContainer>
+								<Title>{item.title}</Title>
+								<Description>{item.desc}</Description>
+								<Button>SHOP NOW</Button>
+							</InfoContainer>
+						</Slide>
+					)
+				})}
+            </Wrapper>
+            <Arrow direction="right" onClick={()=>handleClick("right")}>
+                <ArrowRightOutlined />
+            </Arrow>
+        </Container>
+    )
 }
 
 export default Slider
