e67fd068b6deb5934885915fc5710699bfc814f2
ShivamTaneja <shiivamtaneja@gmail.com>
2023-04-14 14:22:25 +0530

    Add Categories section

--- e-commerce/src/pages/Home.jsx
+++ e-commerce/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Navbar } from '../components/Navbar'
 import Annoucement from '../components/Annoucement'
 import Slider from '../components/Slider'
+import Categories from '../components/Categories'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Annoucement />
       <Navbar />
       <Slider />
+      <Categories />
     </div>
   )
 }
